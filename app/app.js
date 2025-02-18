3949340ba8bed62f89609bbaa815e9300a79f0c4
Paulo Arce <paulodigital.contact@gmail.com>
2025-02-18 20:00:11 -0300

    agregando variables globales al proyecto

---
+++ app/app.js
@@ -0,0 +1,4 @@
+const containerItems = document.getElementById("items-container");
+const productoUsuario = document.getElementById("producto-text");
+const cantidadUsuario = document.getElementById("producto-cantidad");
+const btnAgregar = document.getElementById("btn-agregar");
